e82519503a56607f24d3cfb37cdb72cf4f87700f
Vojdan Stefkovski <vojdan.stefkovski@students.finki.ukim.mk>
2022-10-28 09:51:44 +0200

    lab

--- src/main/java/mk/finki/ukim/mk/lab/web/SelectBalloonServlet.java
+++ src/main/java/mk/finki/ukim/mk/lab/web/SelectBalloonServlet.java
@@ -1,7 +1,15 @@
 package mk.finki.ukim.mk.lab.web;
 
-import javax.servlet.annotation.WebServlet;
+import mk.finki.ukim.mk.lab.service.OrderService;
 
-@WebServlet(urlPatterns = "/selectBalloon")
-public class SelectBalloonServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "select_balloon",urlPatterns = "/selectBalloon")
+public class SelectBalloonServlet extends HttpServlet {
+    private final OrderService orderService;
+
+    public SelectBalloonServlet(OrderService orderService) {
+        this.orderService = orderService;
+    }
 }
